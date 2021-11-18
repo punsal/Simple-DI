9d23ebadcea49b59bde6fdb5530af8e701a3b0ae
Mustafa Pamir Ünsal <p_unsal@msn.com>
2021-11-18 21:43:11 +0000

    Create an example of skilled dragons execution without DI

--- Main.cs
+++ Main.cs
@@ -3,6 +3,48 @@ using System;
 public class Program {
 
   public static void Main(string[] args) {
-    Console.WriteLine("hello world");
+    Console.WriteLine("Dependency Injection Example");
+
+    var dragonFire = new DragonFire();
+    var dragonIce = new DragonIce();
+
+    dragonFire.Skill.Execute();
+    dragonIce.Skill.Execute();
+  }
+}
+
+public class FireSkill{
+  private string _message = "Fire";
+
+  public void Execute(){
+    Console.WriteLine($"Skill is used : {_message}");
+  }
+}
+
+public class IceSkill{
+  private string _message = "Ice";
+
+  public void Execute(){
+    Console.WriteLine($"Skill is used : {_message}");
+  }
+}
+
+public class DragonFire{
+  private FireSkill _skill;
+
+  public FireSkill Skill{get => _skill;}
+
+  public DragonFire(){
+    _skill = new FireSkill();
+  }
+}
+
+public class DragonIce{
+  private IceSkill _skill;
+
+  public IceSkill Skill{get => _skill;}
+
+  public DragonIce(){
+    _skill = new IceSkill();
   }
 }
